bd60fecdeba7c523a3deb24f096d86ef1b6dbf4b
Fatma-Barrani <fatma.BARRANI@esprit.tn>
2026-01-29 11:20:17 +0000

    image docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM {IMAGE}
+FROM eclipse-temurin:17-jdk-alpine
+
 COPY target/student-management-0.0.1-SNAPSHOT.jar student.jar
 EXPOSE 8089
 ENTRYPOINT [ "java", "-jar", "/student.jar" ]
--- src/main/java/tn/esprit/studentmanagement/entities/Department.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Department.java
@@ -20,10 +20,6 @@ public class Department {
     private String phone;
     private String head; // chef de département
 
-    public Department(Long id, String name) {
-        this.idDepartment = id;
-        this.name = name;
-    }
     @OneToMany(mappedBy = "department")
     private List<Student> students;
 }
