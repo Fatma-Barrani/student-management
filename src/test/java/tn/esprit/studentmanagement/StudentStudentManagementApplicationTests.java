bdeb649ba39e86c2c0739ab35640564cf3bdcdf8
Fatma-Barrani <fatma.BARRANI@esprit.tn>
2026-01-29 19:47:52 +0000

    testing

--- src/test/java/tn/esprit/studentmanagement/StudentStudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentStudentManagementApplicationTests.java
@@ -3,10 +3,13 @@ package tn.esprit.studentmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class StudentManagementApplicationTests {
 
-  @Test
+    @Test
     void contextLoads() {
         // Test if Spring Boot context loads successfully
         assertNotNull(this);
